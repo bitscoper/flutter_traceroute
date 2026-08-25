35796786e889b035b870350f61c4f5aa5abafd31
Abdullah As-Sadeed <abdullah_as-sadeed.bd@protonmail.com>
2026-08-25 23:35:18 +0600

    Replaced KGP (Kotlin Gradle Plugin) with Built-In Kotlin; and Formatted Codes.
    Signed-off-by: Abdullah As-Sadeed <abdullah_as-sadeed.bd@protonmail.com>

--- lib/flutter_traceroute_web.dart
+++ lib/flutter_traceroute_web.dart
@@ -3,7 +3,6 @@
 // package as the core of your plugin.
 // ignore: avoid_web_libraries_in_flutter
 import 'dart:html' as html show window;
-import 'dart:js' as js;
 
 import 'package:flutter_traceroute/src/models/traceroute_step.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
